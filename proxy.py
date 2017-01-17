9e3bf12cb502f87582f63ce4cd2148478c1aedbd
xinyi3 <xinyi3@ualberta.ca>
2017-01-17 10:02:28 -0700

    solve busy wait

--- proxy.py
+++ proxy.py
@@ -1,7 +1,7 @@
 #!/user/bin/env python
 
 import socket
-import os
+import os, sys, select
 
 serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -41,8 +41,10 @@ while True:
 				if(part):
 					clientSocket.sendall(part)
 					request.extend(part)
-				else:
+				elif part is None:
 					break
+				else:
+					exit(0)
 
 			if len(request) > 0:	
 				print request
@@ -59,8 +61,16 @@ while True:
 				if(part):
 					incomingSocket.sendall(part)
 					response.extend(part)
-				else:
+				elif part is None:
 					break
+				else:
+					exit(0)
 			if len(response) > 0:	
 				print response
+			select.select(
+				[incomingSocket, clientSocket],   #read
+				[],								  #write
+				[incomingSocket, clientSocket],	  #exception
+				1.0)							  #timeout
+
 
